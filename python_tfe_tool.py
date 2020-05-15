d06fd2b3cf57ef580ccf1fe14cc58c0cd8269fb6
Andrea Zorzetto <andrea.zorzetto@ecs-digital.co.uk>
2020-05-15 11:34:24 +0100

    refactoring

--- python_tfe_tool.py
+++ python_tfe_tool.py
@@ -6,6 +6,7 @@ import getopt
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import os
+import pydoc
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
@@ -22,21 +23,24 @@ def usage(tool_name):
     print('\t--help\t\t\tShow this help message and exit')
     print('\t-h, --hostname\t\tTerraform Enterprise hostname. By default, it uses "app.terraform.io"')
     print('\t-o, --organisation\tOrganization')
+    print('\t-c, --command\t\tCommand name, as for list below.')
     print('\t-w, --workspace\t\tWorkspace name or ID')
+    print('\t-x, --filter\t\tFilter output for a certain name or id. Applicable to vars and workspaces')
     print('\t-v, --variable\t\tNew workspace variable <key:value>')
     print('\t-l, --list\t\tPath to file containing CSV (comma separated) data to use for bulk actions')
-    print('\t-c, --command\t\tCommand name, as for list below.')
     print('\t--credentials\t\tPath to custom TFE credentials file.')
+
     print('\nCommands:')
-    print('\tlist_workspaces\t\tList all Terraform workspaces.')
-    print('\tfind_workspace\t\tFinds either workspace name or ID.\n\t\t\t\tRequire workspace ID, name or file list.')
+    print('\tfind_workspaces\t\tList all Terraform workspaces or a specific workspace ID or Name. Optional filter with ID or name')
+    print('\tfind_vars\t\tList all or filtered vars in the specified workspace. Requires workspace')
     print('\tset_workspace_var\tSet or updates var for specified workspace(s)\n\t\t\t\tRequire workspace ID or name, key_value or file list')
+
     print('\nExamples:')
+    print('\nList all workspaces:')
+    print('\t{0} -o myorg -c find_workspaces '.format(tool_name))
     print('\nFind workspace ID or Name:')
-    print('\t{0} -o myorg -c find_workspace -w ws-L9AQYF1RqRRkQs1k'.format(tool_name))
-    print('\t{0} -o myorg -c find_workspace -w my_workspace'.format(tool_name))
-    print('\nList all available workspaces:')
-    print('\t{0} -o myorg -c list_workspaces'.format(tool_name))
+    print('\t{0} -o myorg -c find_workspaces -x ws-L9AQYF1RqRRkQs1k'.format(tool_name))
+    print('\t{0} -o myorg -c find_workspaces -x my_workspace'.format(tool_name))
     print('\nSet or update workspaces vars:')
     print('\t{0} -o myorg -c set_workspace_var -w my_workspace -v "foo:bar"'.format(tool_name))
     print('\t{0} -o myorg -c set_workspace_var -w my_workspace -l test_data/set_vars.csv'.format(tool_name))
@@ -57,7 +61,36 @@ def get_terraform_token(credentials_file, hostname):
         return json.loads(file_content)["credentials"][hostname]["token"]
 
 
-def list_workspaces(hostname, token, organization):
+def find_vars(hostname, token, organization, workspace, var_filter):
+
+    api_url = "https://{0}/api/v2/vars?filter%5Borganization%5D%5Bname%5D={1}&filter%5Bworkspace%5D%5Bname%5D={2}".format(hostname, organization, workspace)
+
+    headers = {'Content-Type': 'application/vnd.api+json',
+               'Authorization': 'Bearer {0}'.format(token)}
+
+    r = requests.get(api_url, headers=headers, verify=False)
+
+    if r.status_code == 200:
+        all_vars = json.loads(r.content.decode('utf-8'))
+
+        for var in all_vars["data"]:
+            if var_filter is "":
+                return var["id"], var["attributes"]["key"], var["attributes"]["value"]
+            else:
+                if var["id"] == var_filter or var["attributes"]["key"] == var_filter:
+                    return var["id"], var["attributes"]["key"], var["attributes"]["value"]
+
+        print("No vars found with selected criterias/filter")
+        return None
+    elif r.status_code == 404 and r.reason == "Not Found":
+        workspace_name = find_workspaces(hostname, token, organization, workspace)
+        find_vars(hostname, token, organization, workspace_name, var_filter)
+    else:
+        print("{0}, {1}".format(r.status_code, r.reason))
+        return None
+
+
+def find_workspaces(hostname, token, organization, workspace_filter):
 
     api_url = "https://{0}/api/v2/organizations/{1}/workspaces".format(hostname, organization)
 
@@ -67,68 +100,25 @@ def list_workspaces(hostname, token, organization):
     r = requests.get(api_url, headers=headers, verify=False)
 
     if r.status_code == 200:
-        return json.loads(r.content.decode('utf-8'))
-    else:
-        return None
+        all_workspaces = json.loads(r.content.decode('utf-8'))
 
+        if workspace_filter is "":
+            for item in all_workspaces["data"]:
+                print(item["id"], " ", item["attributes"]["name"])
+            return None
 
-# Find either workspace name or ID
-# - checks if the passed value is an ID or a Name
-# - calls find_workspace_id if passed value is a name
-# - calls find_workspace_name if passed value is a id
-def find_workspace(hostname, token, organization, workspace, file_list=""):
-
-    if file_list is "":
-        if workspace is not "":
-            r = find_workspace_id(hostname, token, organization, workspace)
-
-            if r is not None:
-                print(r)
-            else:
-                r = find_workspace_name(hostname, token, workspace)
-                if r is not None:
-                    print(r)
-                else:
-                    print("Workspace {0} not found.".format(workspace))
         else:
-            print("I need a workspace name or id.")
+            for item in all_workspaces["data"]:
+                if item["id"] == workspace_filter:
+                    return item["attributes"]["name"]
 
-    else:
-        with open(file_list) as l:
-            line = l.readline()
-            while line:
-                workspace = line.strip().split(",", 1)[0]
-                find_workspace(hostname, token, organization, workspace)
-                line = l.readline()
+                if item["attributes"]["name"] == workspace_filter:
+                    return item["id"]
 
-
-# Finds ID of the passed Workspace name
-def find_workspace_id(hostname, token, organization, workspace):
-
-    api_url = "https://{0}/api/v2/organizations/{1}/workspaces/{2}".format(hostname, organization, workspace)
-
-    headers = {'Content-Type': 'application/vnd.api+json',
-               'Authorization': 'Bearer {0}'.format(token)}
-    r = requests.get(api_url, headers=headers, verify=False)
-
-    if r.status_code == 200:
-        return json.loads(r.content.decode('utf-8'))["data"]["id"]
-    else:
+        print("No workspace info found with selected criterias/filter")
         return None
-
-
-# Finds Name of the passed Workspace ID
-def find_workspace_name(hostname, token, workspace):
-
-    api_url = "https://{0}/api/v2/workspaces/{1}".format(hostname, workspace)
-
-    headers = {'Content-Type': 'application/vnd.api+json',
-               'Authorization': 'Bearer {0}'.format(token)}
-    r = requests.get(api_url, headers=headers, verify=False)
-
-    if r.status_code == 200:
-        return json.loads(r.content.decode('utf-8'))["data"]["attributes"]["name"]
     else:
+        print(r.reason)
         return None
 
 
@@ -150,9 +140,9 @@ def find_var_id(hostname, token, workspace, varname):
     else:
         return None
 
+
 # Update existing workspace var. Requires var id
 def update_workspace_var(hostname, token, workspace, keyvalue, varid):
-
     api_url = "https://{0}/api/v2/workspaces/{1}/vars/{2}".format(hostname, workspace, varid)
 
     data = {
@@ -184,12 +174,12 @@ def update_workspace_var(hostname, token, workspace, keyvalue, varid):
 # Creates or updates var in workspace(s)
 # If new var, it creates it and set the value
 # If existing var, it calls find_var_id() and update_workspace_var() to update its value
-def set_workspace_var(hostname, token, organization, workspace, keyvalue):
-    keyvalue = keyvalue.split(':', 1)
+def set_workspace_var(hostname, token, organization, workspace, key_value):
+    key_value = key_value.split(':', 1)
 
     # Make sure workspace id is valida, else find workspace id
-    if find_workspace_name(hostname, token, workspace) is None:
-        workspace = find_workspace_id(hostname, token, organization, workspace)
+    if find_workspaces(hostname, token, workspace) is None:
+        workspace = find_workspaces(hostname, token, organization, workspace)
 
     api_url = "https://{0}/api/v2/workspaces/{1}/vars".format(hostname, workspace)
 
@@ -197,8 +187,8 @@ def set_workspace_var(hostname, token, organization, workspace, keyvalue):
         "data": {
             "type": "vars",
             "attributes": {
-                "key": keyvalue[0],
-                "value": keyvalue[1],
+                "key": key_value[0],
+                "value": key_value[1],
                 "category": "terraform",
                 "hcl": False,
                 "sensitive": False
@@ -215,11 +205,11 @@ def set_workspace_var(hostname, token, organization, workspace, keyvalue):
         return json.loads(r.content.decode('utf-8'))
 
     elif r.status_code == 422 and r.json()["errors"][0]["detail"] == "Key has already been taken":
-        print("Key {0} already created. Overwriting with value.".format(keyvalue[0]), keyvalue[1])
-        varid = find_var_id(hostname, token, workspace, keyvalue[0])
+        print("Key {0} already created. Overwriting with value.".format(key_value[0]), key_value[1])
+        varid = find_var_id(hostname, token, workspace, key_value[0])
 
         if varid is not None:
-            return update_workspace_var(hostname, token, workspace, keyvalue, varid)
+            return update_workspace_var(hostname, token, workspace, key_value, varid)
 
     elif r.status_code == 404:
         return r.status_code
@@ -234,10 +224,11 @@ def main(argv):
     file_list = ""
     command = ""
     credentials_file = ""
+    filter = ""
 
     try:
-        opts, args = getopt.getopt(argv, "c:h:w:v:l:o:", ["help", "command=", "hostname=", "workspace=", "variable=",
-                                                          "organization=", "credentials=", "list="])
+        opts, args = getopt.getopt(argv, "c:h:w:v:l:o:x:", ["help", "command=", "hostname=", "workspace=", "variable=",
+                                                            "organization=", "credentials=", "list=", "filter="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -265,36 +256,21 @@ def main(argv):
         elif opt in ("-l", "--list"):
             file_list = arg
 
+        elif opt in ("-x", "--filter"):
+            filter = arg
+
         elif opt in "--credentials":
             credentials_file = arg
 
     api_token = get_terraform_token(credentials_file, hostname)
 
-    if command == "list_workspaces":
-        all_workspaces = list_workspaces(hostname, api_token, organization)
+    if command == "find_workspaces":
+        print(find_workspaces(hostname, api_token, organization, filter))
 
-        if all_workspaces is not None:
-            for item in all_workspaces["data"]:
-                print(item["id"], " ", item["attributes"]["name"])
-        else:
-            print("No workspaces found.")
-
-    elif command == "find_workspace":
-        find_workspace(hostname, api_token, organization, workspace, file_list)
-
-    elif command == "find_workspace_name":
-        w = find_workspace_name(hostname, api_token, workspace)
-        if w is not None:
-            print(w)
-        else:
-            print("Workspace not found.")
-
-    elif command == "find_workspace_id":
-        w = find_workspace_id(hostname, api_token, organization, workspace)
-        if w is not None:
-            print(w)
-        else:
-            print("Workspaces not found.")
+    elif command == "find_vars":
+        a, b, c = find_vars(hostname, api_token, organization, workspace, filter)
+        if a is not None:
+            print("[{0}] {1}: {2}".format(a, b, c))
 
     elif command == "set_workspace_var":
         if file_list is "":
